e89b42202d7b6f8e45f6b64707d0ae032ae4ed97
mahmutyoung <mahmutyoung@gmail.com>
2024-01-09 01:40:19 +0100

    last touches

--- ExpandingCards/script.js
+++ ExpandingCards/script.js
@@ -1,10 +1,19 @@
 const imageList = document.querySelectorAll('.item');
 console.log(imageList[0]);
+const header = document.querySelector('h1');
+console.log(header);
 
-imageList.forEach((image) => {
+imageList.forEach((image, key) => {
+  header.classList.add('visible');
   image.addEventListener('click', () => {
     imageList.forEach((image) => {
       image.classList.remove('active');
+      if (key !== 0) {
+        header.classList.add('hidden');
+        header.classList.remove('visible');
+      } else {
+        header.classList.add('visible');
+      }
     });
     image.classList.add('active');
   });
