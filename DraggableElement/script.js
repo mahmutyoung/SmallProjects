260acb08a930417ec907d1b6dd8fbef5418306b5
mahmutyoung <mahmutyoung@gmail.com>
2024-01-11 22:58:18 +0100

    Draggable element project is done

--- DraggableElement/script.js
+++ DraggableElement/script.js
@@ -1,25 +1,24 @@
+const draggableElement = document.getElementById('mydiv');
+makeElementDraggable(draggableElement);
+
 function makeElementDraggable(element) {
-  let pos1 = 0,
-    pos2 = 0,
-    pos3 = 0,
+  let pos1,
+    pos2,
+    pos3,
     pos4 = 0;
 
-  const dragHeader = element.querySelector('.drag-header');
+  const draggableHeader = document.querySelector('.drag-header');
 
-  dragHeader.addEventListener('mousedown', dragMouseDown);
+  draggableHeader.addEventListener('mousedown', dragMouseDown);
 
   function dragMouseDown(e) {
-    //why do we need preventDefault
-    //https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-    //https://stackoverflow.com/questions/32553158/drag-and-drop-detection-using-html5
     e.preventDefault();
     pos3 = e.clientX;
     pos4 = e.clientY;
     document.addEventListener('mouseup', closeDragElement);
-    document.addEventListener('mousemove', elementDrag);
+    document.addEventListener('mousemove', dragElement);
   }
-
-  function elementDrag(e) {
+  function dragElement(e) {
     e.preventDefault();
     pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
@@ -28,12 +27,8 @@ function makeElementDraggable(element) {
     element.style.top = element.offsetTop - pos2 + 'px';
     element.style.left = element.offsetLeft - pos1 + 'px';
   }
-
   function closeDragElement() {
     document.removeEventListener('mouseup', closeDragElement);
-    document.removeEventListener('mousemove', elementDrag);
+    document.removeEventListener('mousemove', dragElement);
   }
 }
-
-const draggableElement = document.getElementById('mydiv');
-makeElementDraggable(draggableElement);
