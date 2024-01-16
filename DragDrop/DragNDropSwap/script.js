c71f94160fb73043673357897d6afaa937e0518c
mahmutyoung <mahmutyoung@gmail.com>
2024-01-16 22:54:51 +0100

    minor fixes

--- DragDrop/DragNDropSwap/script.js
+++ DragDrop/DragNDropSwap/script.js
@@ -1,9 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   let dragSrcEl = null;
-  console.log(dragSrcEl);
   function handleDragStart(e) {
-    console.log('drag start');
-    console.log(this);
     dragSrcEl = this;
     e.dataTransfer.effectAllowed = 'move';
     e.dataTransfer.setData('text/html', this.innerHTML);
@@ -11,19 +8,16 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function handleDragOver(e) {
-    console.log('drag over');
     e.preventDefault();
     e.dataTransfer.dropEffect = 'move';
     return false;
   }
 
   function handleDragEnter(e) {
-    console.log('drag Enter');
     this.style.opacity = '0.4';
   }
 
   function handleDragLeave(e) {
-    console.log('drag Leave');
     this.style.opacity = '1';
   }
 
@@ -32,14 +26,11 @@ document.addEventListener('DOMContentLoaded', () => {
       this.style.opacity = '1';
       dragSrcEl.innerHTML = this.innerHTML;
       this.innerHTML = e.dataTransfer.getData('text/html');
-      console.log('drop');
     }
   }
 
   function handleDragEnd(e) {
-    console.log('drag end');
     this.style.opacity = '1';
-    console.log(dragSrcEl);
   }
   let items = document.querySelectorAll('.card');
 
