b8be412fcc1ac5899fe6c000d7dbd411e4139594
mahmutyoung <mahmutyoung@gmail.com>
2024-01-16 10:36:03 +0100

    DnD project is done

--- DragDrop/DragNDrop/script.js
+++ DragDrop/DragNDrop/script.js
@@ -1,52 +1,51 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  let dragSrcEl = null;
+let dragSrcEl = null;
 
-  function handleDragStart(e) {
-    console.log('drag start');
-    dragSrcEl = this;
+function handleDragStart(e) {
+  console.log('drag start');
+  dragSrcEl = this;
 
-    e.dataTransfer.effectAllowed = 'move';
-    e.dataTransfer.setData('text/html', this.outerHTML);
-    this.style.opacity = '0.4';
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', this.outerHTML);
+  this.style.opacity = '0.8';
+}
+
+function handleDragOver(e) {
+  console.log('drag over');
+  e.preventDefault();
+  e.dataTransfer.dropEffect = 'move';
+  return false;
+}
+
+function handleDragEnter(e) {
+  console.log('drag Enter');
+  this.style.opacity = '0.4';
+}
+
+function handleDragLeave(e) {
+  console.log('drag Leave');
+  this.style.opacity = '1';
+}
+
+function handleDrop(e) {
+  if (dragSrcEl != this) {
+    this.outerHTML = e.dataTransfer.getData('text/html');
+    dragSrcEl.style.opacity = '1';
+    console.log('drop');
   }
+  draggableItem.outerHTML = '';
+}
 
-  function handleDragOver(e) {
-    console.log('drag over');
-    e.preventDefault();
-    e.dataTransfer.dropEffect = 'move';
-  }
+function handleDragEnd(e) {
+  console.log('drag end');
+  this.style.opacity = '1';
+}
 
-  function handleDragEnter(e) {
-    console.log('drag Enter');
-    this.style.opacity = '0.2';
-  }
+let draggableItem = document.querySelector('.draggable');
+let droppableItem = document.querySelector('.droppable');
 
-  function handleDragLeave(e) {
-    console.log('drag Leave');
-    this.style.opacity = '1';
-  }
-
-  function handleDrop(e) {
-    if (dragSrcEl != this) {
-      this.outerHTML = e.dataTransfer.getData('text/html');
-      dragSrcEl.style.opacity = '0';
-      console.log('drop');
-    }
-  }
-
-  function handleDragEnd(e) {
-    console.log('drag end');
-    this.style.opacity = '1';
-    draggableItem.outerHTML = '';
-  }
-
-  let draggableItem = document.querySelector('.draggable');
-  let droppableItem = document.querySelector('.droppable');
-
-  draggableItem.addEventListener('dragstart', handleDragStart, false);
-  droppableItem.addEventListener('dragenter', handleDragEnter, false);
-  droppableItem.addEventListener('dragover', handleDragOver, false);
-  droppableItem.addEventListener('dragleave', handleDragLeave, false);
-  droppableItem.addEventListener('drop', handleDrop, false);
-  draggableItem.addEventListener('dragend', handleDragEnd, false);
-});
+draggableItem.addEventListener('dragstart', handleDragStart, false);
+droppableItem.addEventListener('dragenter', handleDragEnter, false);
+droppableItem.addEventListener('dragover', handleDragOver, false);
+droppableItem.addEventListener('dragleave', handleDragLeave, false);
+droppableItem.addEventListener('drop', handleDrop, false);
+draggableItem.addEventListener('dragend', handleDragEnd, false);
