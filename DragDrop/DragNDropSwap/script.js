b193c38dc48d75597c80ad4e5b09c59ad3da80a0
mahmutyoung <mahmutyoung@gmail.com>
2024-01-17 01:25:30 +0100

    best practice implementation for Swap Projects

--- DragDrop/DragNDropSwap/script.js
+++ DragDrop/DragNDropSwap/script.js
@@ -4,33 +4,40 @@ document.addEventListener('DOMContentLoaded', () => {
     dragSrcEl = this;
     e.dataTransfer.effectAllowed = 'move';
     e.dataTransfer.setData('text/html', this.innerHTML);
-    this.style.opacity = '0.8';
+
+    this.classList.add('over');
   }
 
   function handleDragOver(e) {
-    e.preventDefault();
+    console.log('drag over');
+    if (e.preventDefault) {
+      e.preventDefault();
+    }
     e.dataTransfer.dropEffect = 'move';
     return false;
   }
 
   function handleDragEnter(e) {
-    this.style.opacity = '0.4';
+    this.classList.add('over');
   }
 
   function handleDragLeave(e) {
-    this.style.opacity = '1';
+    this.classList.remove('over');
   }
 
   function handleDrop(e) {
+    if (e.stopPropagation) {
+      e.stopPropagation();
+    }
     if (dragSrcEl != this) {
-      this.style.opacity = '1';
+      this.classList.remove('over');
       dragSrcEl.innerHTML = this.innerHTML;
       this.innerHTML = e.dataTransfer.getData('text/html');
     }
   }
 
   function handleDragEnd(e) {
-    this.style.opacity = '1';
+    this.classList.remove('over');
   }
   let items = document.querySelectorAll('.card');
 
